40d5a6a668797683ede90c4a3d2f4e1578139fd7
shamaz332 <shamazsaeed332@gmail.com>
2020-11-14 17:18:12 +0500

    toml updated

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/shamaz/web root/12c/.cache/dev-404-page.js"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/home/shamaz/web root/12c/src/pages/index.tsx")))
 }
 
